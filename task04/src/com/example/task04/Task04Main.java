fff51e1a0afedcbc2b324f2c85ad6b9e49dc0c7d
gippster <sajbullin77@gmail.com>
2024-11-15 03:32:04 +0500

    1

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,11 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -9,6 +14,28 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        System.out.println("0.0");
+        SumFloat(System.in);
     }
+    private static void SumFloat(InputStream stream) {
+        double sum = 0.0;
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.ENGLISH);
+        DecimalFormat df = new DecimalFormat("0.000000", symbols);
+
+        try (Scanner scanner = new Scanner(stream)) {
+            scanner.useLocale(Locale.ENGLISH);
+
+            while (scanner.hasNext()) {
+                if (scanner.hasNextDouble()) {
+                    sum += scanner.nextDouble();
+                } else {
+                    scanner.next();
+                }
+            }
+        }
+
+        System.out.print(df.format(sum));
+    }
+
 }
+
